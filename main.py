44114fe339d67883724d4b923c7cb1c976b9c2d8
jessicadba <jessyca.2103@gmail.com>
2025-03-10 14:40:30 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 34
+Nome = "Jessica Souza"
+
+# Print
+print(idade)
+print(Nome)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+print (lista)
+
+# Métodos
+lista_irvertida = lista.reverse()
+
+
