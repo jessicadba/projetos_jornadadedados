4ef12ca7b3999198d29b7f88ebedd774ef79c9a5
jessicadba <jessyca.2103@gmail.com>
2025-03-17 18:36:12 -0300

    atualizando código do desafio 01

--- Labs - Python para Dados/desafio_aula_01/calculo_bonus.py
+++ Labs - Python para Dados/desafio_aula_01/calculo_bonus.py
@@ -1,11 +1,21 @@
 # Definindo as variáveis para a inserção de dados
-nome = input("Informe o seu nome e sobrenome: ")
+nome = input("Informe o seu nome: ")
 salario = float(input("Informe o valor do seu salário bruto: "))
-bonus = float(input("Informe a porcentagem do bônus recebida: "))
+bonus = float(input("Informe o valor do bônus recebido: "))
+#CONSTANTE_BONUS = salario * 4.5
+# Uma constante é uma variável cujo valor não deve ser alterado após ser definido. As constantes em python, são definidas com LETRAS MAIÚSCULAS.
+# Se o bônus for um valor fixo, ou seja, que não irá mudar, pode-se definir a CONSTANTE_BONUS acima.
+ 
 
 # Calculando o valor do bônus
-valor_bonus = salario * (bonus / 100)
+# Aqui pode-se alterar a regra de cálculo do bônus, de acordo com as regras de negócio da empresa.
+valor_bonus = salario * (bonus / 100) # ajustado para calcular em porcentagem.
+#valor_bonus = CONSTANTE_BONUS
 
-# Exibindo o resultado. O f-strings para formatar o resultado com duas casas decimais (:.2f), tornando o valor mais legível.
-print(f"O valor do seu bônus é: {valor_bonus:.2f}")
+
+# Exibindo o resultado. 
+# O "f" f-strings permite colocar texto e as variáveis juntas, {}
+# (:.2f) para formatar o resultado com duas casas decimais, tornando o valor mais legível.
+print(f"{nome}, o valor do seu bônus será de: {valor_bonus:.2f}")
+
 
